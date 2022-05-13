12c6d7fc1625317d52b9ce36ddc2796c39a0b183
Zara <fati18flora@gmail.com>
2022-05-12 22:22:45 -0400

    Updates

--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Code } from './code/Code';
-export {default as Features } from './features/Features';
-export {default as Team } from './team/Team';
-export {default as Header } from './header/Header';
-export {default as Documentation } from './documentation/Documentation';
-export {default as WhatRHGAN } from './whatRHGAN/WhatRHGAN';
